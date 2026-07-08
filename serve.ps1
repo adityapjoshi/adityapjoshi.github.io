a17718b1b864262a19ca4896d90ec94bf60290f0
Aditya Prakash Joshi <adityapjoshi@gmail.com>
2026-07-08 14:09:35 +1200

    removed the extract spacing between the sections

---
+++ serve.ps1
@@ -0,0 +1,28 @@
+$listener = [System.Net.HttpListener]::new()
+$listener.Prefixes.Add('http://127.0.0.1:8000/')
+$listener.Start()
+
+try {
+  while ($listener.IsListening) {
+    $context = $listener.GetContext()
+    $path = $context.Request.Url.LocalPath.TrimStart('/')
+    if ([string]::IsNullOrWhiteSpace($path)) { $path = 'index.html' }
+    $full = Join-Path 'C:\Dev\Repos\adityapjoshi.github.io' $path
+
+    if (Test-Path $full -PathType Leaf) {
+      $bytes = [System.IO.File]::ReadAllBytes($full)
+      if ($full.EndsWith('.html')) { $context.Response.ContentType = 'text/html; charset=utf-8' }
+      elseif ($full.EndsWith('.css')) { $context.Response.ContentType = 'text/css; charset=utf-8' }
+      elseif ($full.EndsWith('.js')) { $context.Response.ContentType = 'application/javascript; charset=utf-8' }
+      $context.Response.OutputStream.Write($bytes, 0, $bytes.Length)
+    } else {
+      $context.Response.StatusCode = 404
+    }
+
+    $context.Response.OutputStream.Close()
+  }
+}
+finally {
+  $listener.Stop()
+  $listener.Close()
+}
